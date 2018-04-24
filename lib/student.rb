ceb2238b2144126d2401f269797b464caee0fb51
Rebecca Tong <rtong@buffalo.edu>
2018-04-24 00:41:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,31 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+attr_accessor :id, :name, :grade
+
+  def initialize(id,name)
+    @id = nil
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+
+  def self.drop_table
+
+  end
+
+ # -- Remember, you can access your database connection anywhere in this class  with DB[:conn]
 
 
 end
