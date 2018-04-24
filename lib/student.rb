b021e847caa407df7bec3253ae653e9d69942476
Rebecca Tong <rtong@buffalo.edu>
2018-04-24 00:46:00 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,6 +32,12 @@ attr_accessor :id, :name, :grade
  # -- Remember, you can access your database connection anywhere in this class  with DB[:conn]
 
  def save
+   sql = <<-SQL
+   INSERT INTO students(name,grade)
+   VALUES(?,?)
+   SQL
+   DB[:conn].execute(sql,self.name,self.grade)
+
  end
 
  def self.create
