440bfa11d69ae503f40776948a987d14e25821f6
Rebecca Tong <rtong@buffalo.edu>
2018-04-24 01:01:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,11 +48,7 @@ attr_accessor :id, :name, :grade
  end
 
  def self.new_from_db(row)
-   new_student = Student.new
-   new_student.id = row[0]
-   new_student.name = row[1]
-   new_student.grade = row[2]
-   new_student
+    Student.new(row[0], row[1], row[2])
  end
 
  def self.find_by_name
