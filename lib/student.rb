c6e5220026b940802cb693236bcadf093ed84eb1
Rebecca Tong <rtong@buffalo.edu>
2018-04-24 00:49:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,7 +40,7 @@ attr_accessor :id, :name, :grade
    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
  end
 
- def self.create
+ def self.create(name,grade)
    student = Student.new(name,grade)
    student.save
    student
