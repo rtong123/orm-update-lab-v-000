e6b0fef497b1257af0626a1178dece1b4afa312d
Rebecca Tong <rtong@buffalo.edu>
2018-04-24 01:06:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,12 +33,16 @@ attr_accessor :id, :name, :grade
  # -- Remember, you can access your database connection anywhere in this class  with DB[:conn]
 
  def save
-   sql = <<-SQL
-   INSERT INTO students(name,grade)
-   VALUES(?,?)
-   SQL
-   DB[:conn].execute(sql,self.name,self.grade)
-   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+   if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
  end
 
  def self.create(name,grade)
