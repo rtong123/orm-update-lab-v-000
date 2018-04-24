94a421f60ae6f3833e10f421902e31ac6c7b8b3f
Rebecca Tong <rtong@buffalo.edu>
2018-04-24 00:42:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,7 +23,10 @@ attr_accessor :id, :name, :grade
 
 
   def self.drop_table
+    sql = <<-SQL
     DROP TABLE students
+    SQL
+    DB[:conn].execute(sql)
   end
 
  # -- Remember, you can access your database connection anywhere in this class  with DB[:conn]
