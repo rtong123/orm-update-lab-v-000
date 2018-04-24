dbbbd4fd974efab8fe91e93ec592617e27ae8db6
Rebecca Tong <rtong@buffalo.edu>
2018-04-24 00:44:25 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,5 +31,16 @@ attr_accessor :id, :name, :grade
 
  # -- Remember, you can access your database connection anywhere in this class  with DB[:conn]
 
+ def save
+ end
+
+ def self.create
+ end
+
+ def self.new_from_db
+ end
+
+ def self.find_by_name
+ end
 
 end
