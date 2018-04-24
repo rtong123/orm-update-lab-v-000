10f82333f777b1da9bccfb355c0772eccc693940
Rebecca Tong <rtong@buffalo.edu>
2018-04-24 00:46:56 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,6 +37,7 @@ attr_accessor :id, :name, :grade
    VALUES(?,?)
    SQL
    DB[:conn].execute(sql,self.name,self.grade)
+   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
 
  end
 
